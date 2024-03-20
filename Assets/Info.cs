a3099d501a45a28ca060fb294930ccc12846114e
Myles <mylescrockem@gmail.com>
2024-03-20 17:48:51 -0500

    added button randomization

--- Assets/Info.cs
+++ Assets/Info.cs
@@ -27,6 +27,27 @@ public class Info : MonoBehaviour
             case 0:
                 buttonSp.sprite = SpriteArray[0];
                 break;
+            case 1:
+                buttonSp.sprite = SpriteArray[1];
+                break;
+            case 2:
+                buttonSp.sprite = SpriteArray[2];
+                break;
+            case 3:
+                buttonSp.sprite = SpriteArray[3];
+                break;
+            case 4:
+                buttonSp.sprite = SpriteArray[4];
+                break;
+            case 5:
+                buttonSp.sprite = SpriteArray[5];
+                break;
+            case 6:
+                buttonSp.sprite = SpriteArray[6];
+                break;
+            case 7:
+                buttonSp.sprite = SpriteArray[7];
+                break;
         }
     }
 }
